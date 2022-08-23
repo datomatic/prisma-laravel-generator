445920d3e2a021a43c9d4e97c5a4f39109a07dca
Roberto Negro <negro.roberto2@gmail.com>
2022-08-23 15:42:49 +0200

    feat: timestamps default by name

--- packages/generator/src/helpers/is-field-deleted-timestamp.ts
+++ packages/generator/src/helpers/is-field-deleted-timestamp.ts
@@ -2,6 +2,7 @@ import {DMMF} from '@prisma/generator-helper';
 import findOnDocumentation from '../utils/find-on-documentation';
 
 const isFieldDeletedTimestamp = (field: DMMF.Field) =>
+  field.name === 'deleted_at' ||
   findOnDocumentation('deleted_at', field.documentation);
 
 export default isFieldDeletedTimestamp;
--- packages/generator/src/helpers/is-field-updated-timestamp.ts
+++ packages/generator/src/helpers/is-field-updated-timestamp.ts
@@ -2,6 +2,8 @@ import {DMMF} from '@prisma/generator-helper';
 import findOnDocumentation from '../utils/find-on-documentation';
 
 const isFieldUpdatedTimestamp = (field: DMMF.Field) =>
-  findOnDocumentation('updated_at', field.documentation) || !!field.isUpdatedAt;
+  field.name === 'updated_at' ||
+  findOnDocumentation('updated_at', field.documentation) ||
+  !!field.isUpdatedAt;
 
 export default isFieldUpdatedTimestamp;
