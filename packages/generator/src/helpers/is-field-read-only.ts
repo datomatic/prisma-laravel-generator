0383695690083cde1a46ca64ff115cac26acd0eb
Roberto Negro <negro.roberto2@gmail.com>
2022-08-24 18:15:22 +0200

    feat: created_at, updated_at and deleted_at considered as read-only

--- packages/generator/src/helpers/is-field-read-only.ts
+++ packages/generator/src/helpers/is-field-read-only.ts
@@ -1,7 +1,14 @@
 import {DMMF} from '@prisma/generator-helper';
+import _ from 'lodash';
 import findOnDocumentation from '../utils/find-on-documentation';
+import isFieldUpdatedTimestamp from './is-field-updated-timestamp';
+import isFieldCreatedTimestamp from './is-field-created-timestamp';
+import isFieldDeletedTimestamp from './is-field-deleted-timestamp';
 
 const isFieldReadOnly = (field: DMMF.Field) =>
-  findOnDocumentation('read-only', field.documentation);
+  findOnDocumentation('read-only', field.documentation) ||
+  isFieldCreatedTimestamp(field) ||
+  isFieldUpdatedTimestamp(field) ||
+  isFieldDeletedTimestamp(field);
 
 export default isFieldReadOnly;
