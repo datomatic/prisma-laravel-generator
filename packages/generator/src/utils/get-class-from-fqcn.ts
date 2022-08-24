0950fc503eb35ab9c1a1b3b23f4c494fc20ab805
Roberto Negro <negro.roberto2@gmail.com>
2022-08-24 20:56:35 +0200

    fix: alias "as" case insensitive match

--- packages/generator/src/utils/get-class-from-fqcn.ts
+++ packages/generator/src/utils/get-class-from-fqcn.ts
@@ -1,4 +1,9 @@
 const getClassFromFQCN = (fqcn: string) => {
+  const asMatch = /\s+as\s+/gi.exec(fqcn);
+  if (asMatch) {
+    return fqcn.slice(asMatch.index + 4).trim();
+  }
+
   if (fqcn.includes('\\')) {
     return fqcn.slice(fqcn.lastIndexOf('\\') + 1);
   }
